0a17a07e49c78ac072196d345f751c23cb41b4ef
Aiden Janey <arjaney.professional@gmail.com>
2024-06-26 10:11:33 -0400

    Refactoring (Using MERN stack instead of Flask)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'
+import Home from './pages/Home'
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/home',
+    element: <Home />
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
