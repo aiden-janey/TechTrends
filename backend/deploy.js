8900092dc0265fd04b9cf0194875bdeea0857da9
aiden-janey <arjaney.professional@gmail.com>
2024-08-04 20:58:24 -0400

    Enable CORS

--- backend/deploy.js
+++ backend/deploy.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const userController = require("./controllers/userController.js");
 const jobController = require("./controllers/jobController.js");
 const app = express();
-//app.use(cors());
-app.use(bodyParser.json());
 
 mongoose
   .connect("mongodb://localhost:27017/techtrends")
@@ -15,6 +14,9 @@ mongoose
   .catch((err) =>
     console.log(`Error in DB Connection: ${JSON.stringify(err, undefined, 2)}`)
   );
+
+app.use(cors());
+app.use(express.json());
 
 app.use("/users", userController);
 app.use("/jobs", jobController);
