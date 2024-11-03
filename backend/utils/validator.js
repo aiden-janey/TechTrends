34d2b29e14fcafe211041a818bcb568dd2db1799
Aiden Janey <arjaney.professional@gmail.com>
2024-11-02 20:38:10 -0400

    User Validation Complete

--- backend/utils/validator.js
+++ backend/utils/validator.js
@@ -12,8 +12,28 @@ const validator = {
     return re.test(password);
   },
   validateId: (id) => {
-    let re = new RegExp("");
+    let re = new RegExp("[a-zA-z0-9]{16}$");
     return re.test(id);
+  },
+  validateSalary: (salary) => {
+    let re = new RegExp("^[0-9]{1,8}(.[0-9]{1,2})?$");
+    return re.test(salary);
+  },
+  validateAge: (age) => {
+    let re = new RegExp("^(?:[1-9]?[0-9]|1[01][0-9]|120)$");
+    return re.test(age);
+  },
+  validateCountry: (country) => {
+    let re = new RegExp("^[A-Za-z]+(?:[ -][A-Za-z]+)*$");
+    return re.test(country);
+  },
+  validatePosition: (position) => {
+    let re = new RegExp("^[A-Za-z]+(?:[ -][A-Za-z]+)*$");
+    return re.test(position);
+  },
+  validateLevel: (level) => {
+    let re = new RegExp("^[A-Za-z]+(?:[ -][A-Za-z]+)*$");
+    return re.test(level);
   },
 };
 
